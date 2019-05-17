11fcb84083b11cf77a06fc569fa14aefcdbe0021
leif.van.holland@fkie.fraunhofer.de <leif.van.holland@fkie.fraunhofer.de>
2019-05-17 11:33:02 +0200

    Prepare for module export

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='sciutils',
     version='0.1dev',
-    packages=[''],
-    url='',
+    packages=['sciutils'],
+    url='https://github.com/leifvan/sciutils',
     license='Unlicense',
     author='leif.van.holland',
     author_email='',
